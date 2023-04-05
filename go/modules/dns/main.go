886321c73470406772a1e3be0f7a0a5420935266
Tim Hiatt <timhiatt@google.com>
2023-04-05 15:00:39 +0000

    fleshing out DNS

--- go/modules/dns/main.go
+++ go/modules/dns/main.go
@@ -13,3 +13,110 @@
 // limitations under the License.
 
 package dns
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+
+func NewDNS(ctx *pulumi.Context, name string, args DNSArgs, opts pulumi.ResourceOption) (state *DNSState, err error) {
+
+	err := validate.Struct(args)
+	validationErrors := err.(validator.ValidationErrors)
+	
+	// Structure Load is Completed HERE
+
+	for key, attrs := range(args.RecordSets){
+		
+	}
+
+	/*_recordsets_0 = {
+		for key, attrs in var.recordsets :
+		key => merge(attrs, zipmap(["type", "name"], split(" ", key)))
+	}*/
+
+	
+	/* Handling TF Locals */ 
+
+
+
+
+	locals {
+		# split record name and type and set as keys in a map
+		_recordsets_0 = {
+		  for key, attrs in var.recordsets :
+		  key => merge(attrs, zipmap(["type", "name"], split(" ", key)))
+		}
+		# compute the final resource name for the recordset
+		_recordsets = {
+		  for key, attrs in local._recordsets_0 :
+		  key => merge(attrs, {
+			resource_name = (
+			  attrs.name == ""
+			  ? var.domain
+			  : (
+				substr(attrs.name, -1, 1) == "."
+				? attrs.name
+				: "${attrs.name}.${var.domain}"
+			  )
+			)
+		  })
+		}
+		# split recordsets between regular, geo and wrr
+		geo_recordsets = {
+		  for k, v in local._recordsets :
+		  k => v
+		  if v.geo_routing != null
+		}
+		regular_recordsets = {
+		  for k, v in local._recordsets :
+		  k => v
+		  if v.records != null
+		}
+		wrr_recordsets = {
+		  for k, v in local._recordsets :
+		  k => v
+		  if v.wrr_routing != null
+		}
+		zone = (
+		  var.zone_create
+		  ? try(
+			google_dns_managed_zone.non-public.0, try(
+			  google_dns_managed_zone.public.0, null
+			)
+		  )
+		  : try(data.google_dns_managed_zone.public.0, null)
+		)
+		dns_keys = try(
+		  data.google_dns_keys.dns_keys.0, null
+		)
+	  }
+
+
+
+
+	
+	/* Two Standard Routines
+		 - Get Defaults
+		 - Suplement Defaults (Compare incoming, add missing, exclude overidden Defaults)
+		 - Struct with Perfect Values... Run Build. 
+
+
+		 - Two YAMLs (Defaults & Data) ---> Merged Sruct Per Data YAML. Execute.
+		 - 
+	*/
+
+
+	//Resource google_dns_managed_zone (Non-Public)
+
+	//Resource google_dns_managed_zone (Public)
+
+	//Resource google_dns_record_set (cloud static records)
+
+	//Resource google_dns_record_set (cloud geo records)
+
+	//Resource google_dns_record_set (cloud wrr records)
+
+	return state, err
+}
