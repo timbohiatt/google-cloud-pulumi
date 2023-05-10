4702c6816c5a8ddcabd855d9104de406025e0b7c
Tim Hiatt <timhiatt@google.com>
2023-05-10 18:46:04 +0000

    Adding some additional base modules

--- go/blueprints/project-factory/main.go
+++ go/blueprints/project-factory/main.go
@@ -20,7 +20,7 @@ import (
 	"github.com/pulumi/pulumi-gcp/sdk/v6/go/gcp"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
-	projectfactory "github.com/timbohiatt/google-cloud-pulumi/go/factories/project"
+	projectfactory "github.com/timbohiatt/google-cloud-pulumi/go/factories/project-factory"
 	project "github.com/timbohiatt/google-cloud-pulumi/go/modules/project"
 )
 
